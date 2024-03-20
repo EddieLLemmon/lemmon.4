95039fceb47e234d84d2cda98ad3da89fac28bbc
Eddie Lemmon <eddielemmon@outlook.com>
2024-03-19 20:48:04 -0500

    Added queue data, and added more info about terminating process

--- oss.c
+++ oss.c
@@ -12,6 +12,10 @@
 #include <sys/shm.h>
 
 #define PERMS 0644
+#define HP 10000000 // Milliseconds for Highest Priority
+#define MP 20000000 //Middle Priority
+#define LP 40000000 //Lowest Priority
+
 
 void help(void); //Help function for if the user prints out help.
 void incrementClock(int*, int, int*, int); //The system clock used by both programs.
@@ -23,7 +27,7 @@ void printTable(int*, FILE*);
 
 struct PCB 
  {
-   int occupied;
+   int occupied; //If the process is table is occupied
    pid_t pid;
    int startSeconds;
    int startNano;
@@ -32,12 +36,20 @@ struct PCB
    int eventBlockedUntilNano; // when will this process become unblocked
  };
  
+struct Queue //Queue struct for processes
+{
+ int pid;
+ double priority;
  
+}; 
+
+
  
 struct PCB processTable[20];
 
 int main(int argc, char** argv)
 {
+ Queue *q0, *q1, *q2;
 }
 
 void help(void) //Help function 
@@ -49,4 +61,44 @@ void help(void) //Help function
   printf("The # after -t is the maximum bound of time that a child processes can be launched for\n");
   printf("The # after -i is repesents how many milliseconds a child should wait before launching \n");
   printf("filename represents the name of the file that will be used log information about the file\n");
+ }
+ 
+ void incrementClock(int* shm, int i, int*nanoholder, int sc)
+ {
+ 
+  shm[1] += 1000
+ 
+ 
+  if(abs(shm[1] - *nanoholder) >= 1000000) //One million nanoseconds is equal to a millisecond.
+   {
+     shm[2] += 1;
+     *nanoholder = shm[1];
+     printf("%d\n", shm[2]);
+   }
+   
+  if (shm[2] >= i) //Milliseconds are used to control how often a child is launched.
+   {
+    childready = true;
+    shm[2] = 0; //Millseconds reset after a certain amount of time.
+   }
+  
+  if (shm[1] >= 1000000000) //One billion nanoseconds equals one second.
+  {
+    shm[0] += 1;
+    shm[1] = 0;
+  }
+ }
+ 
+ int getRandomsecs(int upper) //Prooduces a truly random number of seconds based on the time because of srand
+ {
+  srand(time(NULL));
+  int j = (rand() % (upper - 1 + 1)) + 1;
+  return j;
+  
+ }
+ int getRandomnanos(void)
+ {
+  srand(time(NULL));
+  int j = (rand() % (999999999 - 1 + 1)) + 1;
+  return j;
  }
--- worker.c
+++ worker.c
@@ -7,10 +7,14 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/msg.h>
+#include <time.h>
+#include <stdbool.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
 #define PERMS 0644
+
+int termearly(void);
 
 typedef struct msgbuffer {
  int intData;
@@ -22,3 +26,11 @@ int main(int argc, char **argv)
 {
  
 }
+
+
+int termearly(void)
+{
+   srand(time(NULL));
+   
+   
+} 
